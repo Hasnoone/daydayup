9737e090c00c5e0a2020c32056cc61c2fb9241f7
xuhang <daguji2300@gmail.com>
2021-12-14 09:19:10 +0800

    NIO demo

---
+++ NIO/src/main/java/org/example/CreateBufferDemo.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.nio.ByteBuffer;
+
+public class CreateBufferDemo {
+
+
+    public static void main(String[] args) {
+        //创建置顶长度的缓冲区 byteBuffer为例
+        ByteBuffer allocate = ByteBuffer.allocate(5);
+
+
+    }
+
+
+}
