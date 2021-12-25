ebf4e59a1c91d546dcb21383cdde39285b524dbc
xuhang <daguji2300@gmail.com>
2021-12-25 10:01:00 +0800

    缓冲区的创建

--- NIO/src/main/java/org/example/CreateBufferDemo.java
+++ NIO/src/main/java/org/example/CreateBufferDemo.java
@@ -8,6 +8,25 @@ public class CreateBufferDemo {
     public static void main(String[] args) {
         //创建置顶长度的缓冲区 byteBuffer为例
         ByteBuffer allocate = ByteBuffer.allocate(5);
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(allocate.get());//从缓冲区拿去数据
+//        }
+//        System.out.println(allocate.get());//这时候就会报错、
+
+
+
+        //创建一个有内容的缓冲区
+        byte[] bytes = new byte[1024];
+        String content = "hello";
+        ByteBuffer wrap = ByteBuffer.wrap(content.getBytes());
+        for (int i = 0; i < content.length(); i++) {
+
+            System.out.println(wrap.get());
+
+
+        }
+
+
 
 
     }
